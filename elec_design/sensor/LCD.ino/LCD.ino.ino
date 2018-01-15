56edd24417c02f105f9061e6e6df82e505b4a820
40023139 <40023139@gm.nfu.edu.tw>
2018-01-15 16:18:40 +0800

    Add files via upload

---
+++ elec_design/sensor/LCD.ino/LCD.ino.ino
@@ -0,0 +1,22 @@
+#include "U8glib.h"
+ 
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_FAST); //設定LCD面板，根據面板尺寸設定
+int i=0;
+ 
+void setup(void) {
+  u8g.setFont(u8g_font_5x8); // font instellen.
+  pinMode(8, INPUT);
+}
+ 
+void loop(void) {
+  if(digitalRead(8)){
+    i++;
+  }
+  u8g.firstPage();  
+  do {
+  u8g.setFont(u8g_font_unifont);
+  u8g.setPrintPos(0, 20); //設定顯示位置
+    u8g.print(i);
+    } while( u8g.nextPage() );
+  delay(100); // rebuild the picture after some delay
+}
---
+++ elec_design/stepmotor/step/step.ino
@@ -0,0 +1,34 @@
+#include <Stepper.h>
+#define STEPS 2048
+
+
+Stepper stepper(STEPS, 8, 10, 9, 11);
+
+void setup()
+{
+stepper.setSpeed(5);     // 將馬達的速度設定成150RPM 最大  150~160
+}
+
+void loop()
+{
+Serial.begin(9600);
+Serial.println("+0.5"); //透過序列視窗看到馬達現在轉角
+stepper.step(2048);//正半圈
+delay(1);
+/*stepper.step(-1024);//反半圈
+delay(1000);
+Serial.println("-0.5"); //透過序列視窗看到馬達現在轉角
+stepper.step(2048);//正1圈
+delay(1000);
+stepper.step(-2048);//反1圈
+delay(1000);
+stepper.step(3072);//正1圈半
+delay(1000);
+stepper.step(-3072);//反1圈半
+delay(1000);
+stepper.step(4096);//正2圈
+delay(1000);
+stepper.step(-4096);//反2圈
+delay(1000);*/
+}
+
